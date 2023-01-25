e49287e12fe3c77aebf5822e13fcae5ab4c38ef0
Code Preceptor <122684335+codepreceptor@users.noreply.github.com>
2023-01-25 22:18:11 +0530

    Update hacktools.py

--- hacktools.py
+++ hacktools.py
@@ -1,11 +1,5 @@
 import os
-
-require = '''echo {} | pkg up
-pip install colorama lolpython
-pkg install python figlet'''
-
-os.system(require)
-import  time,webbrowser
+import  time
 from colorama import Fore
 from lolpython import lol_py 
 
@@ -17,10 +11,10 @@ def clear():
     os.system("clear")
 
 def logo():
-     print(Fore.RED+"=======================================================")
+     print(Fore.RED+"======================================================")
      text = "echo {}| figlet HACKTOOLS | lolpython"
      os.system(text)
-     print(Fore.RED+"===================== version 1.0 =====================\n")
+     print(Fore.RED+"===================== version 1.0 ====================\n")
 
 
 def about_creator():
@@ -33,7 +27,7 @@ def about_creator():
     lol_py(telegram)
     lol_py(github)
     lol_py(youtube)
-    print(Fore.GREEN+"=======================================================\n\n")
+    print(Fore.GREEN+"======================================================\n\n")
 
 def option():
     options = '''    [1] VULNERABILITY SCANNERS (nmap, etc..) 
@@ -60,20 +54,23 @@ def option_1():
         user = int(user_input)
         
         if user == 1:
-            option_1()
+            nmap = "pkg install nmap"
+            os.system(nmap)        
+            print("Installation Successful. Type nmap --help for getting help.")
         elif user == 2:
-            option_2()
+            sqlmap = "cd $HOME && git clone --depth 1 https://github.com/sqlmapproject/sqlmap"
+            os.system(sqlmap)
+            print("Installation Successful. Type cd sqlmap && python sqlmap.py -h to get Help.")
         elif user == 3:
-            option_3()
+            tmscanner = " cd $HOME && apt update && apt upgrade && apt install git python2 && git clone https://github.com/TechnicalMujeeb/TM-scanner.git && cd TM-scanner && chmod +x * && sh install.sh"
+            os.system(tmscanner)
+            print("Installation Successful. Type cd TM-scanner && python2 tmscanner.py to Start.")
         elif user == 4:
-            clear()
-            logo()
-            about_creator()
-            option()
-            choose()
+            home()
         elif user == 5:
             option_0()
         else:
+            logo()
             option_1()
             
     except ValueError:
@@ -81,31 +78,154 @@ def option_1():
         Please Wait for 3 Seconds''')
         time.sleep(3)
         os.system("clear")
+        logo()
+        about_creator()
         option_1()
     
 def option_2():
-    options_2 = '''    [1] Metasploit Framework
-        '''
-    lol_py(options_2)
-    
+    options_21= '''    [1] Metasploit Framework
+    [2] BACK
+    [0] EXIT'''
+    lol_py(options_21)
+    user_input =  input(Fore.YELLOW+"Choose Option: ")
+    try:
+        user = int(user_input)
+        if user == 1:
+            metasploit = ''' cd $HOME && pkg update
+            pkg install wget -y
+            wget https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
+            bash metasploit.sh
+            rm metasploit.sh
+            cd metasploit-framework
+            bundle install '''
+            os.system(metasploit )
+            
+        elif user == 0:
+            option_0()
+        
+        elif user == 2:
+            home()
+            
+        else:
+            logo()
+            about_creator()
+            option_2()
+            
+    except ValueError:
+            print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+            time.sleep(3)
+            os.system("clear")
+            logo()
+            about_creator()
+            option_2()
     
 def option_3():
-    options_3 = '''    [1] XBomb
+    options_3_1 = '''    [1] XBomb
+    [2] BACK
+    [3] EXIT
         '''
-    lol_py(options_3)
+    lol_py(options_3_1)
+    user_input =  input(Fore.YELLOW+"Choose Option: ")
+    try:
+        user = int(user_input)
+        if user == 1:
+            xbomb = "cd $HOME && git clone https://github.com/mr-exo/XBomb.git"
+            os.system(xbomb)
+            print("Installation Successful. Type cd XBomb && pip install -r requirements.txt && python XBomb.py to Start")
+            
+        elif user == 2:
+            home()
+         
+        elif user == 0:
+            option_0()
+        
+        else:
+            logo()
+            about_creator()  
+            option_3()
+            
+    except ValueError:
+            print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+            time.sleep(3)
+            os.system("clear")
+            logo()
+            about_creator()
+            option_3()                
     
           
 def option_4():
-    options_4 = '''    [1] Zphisher
-        '''
-    lol_py(options_4)
-    
+    options_4_1= '''    [1] Zphisher
+    [2] BACK
+    [0] EXIT        '''
+    lol_py(options_4_1)
+    user_input = input(Fore.YELLOW+"Choose Option: ")
+    try :
+          user = int(user_input)
+          if user == 1:
+              zphisher = '''cd $HOME && pkg install tur-repo
+ pkg install zphisher
+ zphisher''' 
+              os.system(zphisher)
+              print("Installation Successful. Type zphisher to Start")
+          
+          elif user == 2:
+              home()
+           
+          elif user == 0:
+              option_0()
+           
+          else:
+              logo()
+              about_creator()
+              option_4()
+              
+    except ValueError:
+            print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+            time.sleep(3)
+            os.system("clear")
+            logo()
+            about_creator()
+            option_4()                               
           
 def option_5():
-    options_5 = '''    [1] Ip-tracer
-        '''
-    lol_py(options_5)
+    options_5_1 = '''    [1] IP-Tracer
+    [2] BACK
+    [0] EXIT        '''
+    lol_py(options_5_1)
     
+    user_input = input(Fore.YELLOW+"Choose Option: ")
+    try :
+          user = int(user_input)
+          if user == 1:
+              ip = '''cd $HOME && git clone https://github.com/rajkumardusad/IP-Tracer.git
+cd IP-Tracer
+chmod +x install
+sh install '''
+              
+              os.system(ip)
+              print("Installation Successful. Type trace -t target-ip to Trace.")
+          elif user == 2:
+             home()
+             
+          elif user == 0:
+             option_0()
+             
+          else:
+             logo()
+             about_creator()
+             option_5()
+
+    except ValueError:
+            print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+            time.sleep(3)
+            os.system("clear")
+            logo()
+            about_creator()
+            option_5()                  
     
 def option_0():
     os.system("clear")
@@ -114,9 +234,9 @@ def option_0():
     wait()
     about_creator()
     wait()
-    thanks = (f"      THANKS FOR USING MY TOOL.\n")
-    please = (f"      PLEASE DON'T FORGOT TO JOIN MY YOUTUBE AND TELEGRAM CHANNELS.\n")
-    havean = (f"      HAVE A NICE DAY.\n\n") 
+    thanks = (f"                      THANKS FOR USING MY TOOL.\n")
+    please = (f"      PLEASE🙏 DON'T FORGOT TO JOIN MY YOUTUBE AND TELEGRAM CHANNELS.\n")
+    havean = (f"                      HAVE A NICE DAY😊.\n\n") 
     
     lol_py(thanks)
     wait()
@@ -157,7 +277,7 @@ def choose():
         Please Wait for 3 Seconds''')
         time.sleep(3)
         os.system("clear")
-        return home()
+        home()
     
         
 def home():
