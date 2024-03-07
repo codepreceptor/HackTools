c7fd32d8278a367e8fb109c1cc89c08a1f6a0cd0
Code Preceptor <122684335+codepreceptor@users.noreply.github.com>
2024-03-07 22:32:36 +0530

    Update hacktools.py

--- hacktools.py
+++ hacktools.py
@@ -1,121 +1,34 @@
-#DON'T COPY IT WITHOUT CREDIT.
-#OUR TELEGRAM CHANNEL: https://t.me/codepreceptor
-
-
-import os
-import time
+import os,time
 from colorama import Fore
-from lolpython import lol_py
+from lolpython import lol_py 
 
-update_message = Fore.GREEN + "Tool Update Successful. "
 
-def execute_command(command, success_message):
-    os.system(command)
-    print(success_message)
+update = Fore.GREEN+"Tool Update Successful. "
 
-def clear_screen():
+def wait():
+    time.sleep(1.2)
+    
+def clear():
     os.system("clear")
 
-def display_header():
-    print(Fore.RED + "======================================================")
-    text = "echo {}| figlet HACKTOOLS | lolpython"
-    os.system(text)
-    print(Fore.RED + "===================== version 1.2 ====================\n")
+def logo():
+     print(Fore.RED+"======================================================")
+     text = "echo {}| figlet HACKTOOLS | lolpython"
+     os.system(text)
+     print(Fore.RED+"===================== version 1.2 ====================\n")
 
-def display_about_creator():
+
+def about_creator():
     creator = "Tool Creator: Code Preceptor"
     telegram = "Telegram Channel: https://t.me/codepreceptor"
     github = "GitHub Profile: https://github.com/codepreceptor"
     youtube = "Youtube Channel: https://youtube.com/@codepreceptor\n"
-
+    
     lol_py(creator)
     lol_py(telegram)
     lol_py(github)
     lol_py(youtube)
-    print(Fore.GREEN + "======================================================\n\n")
-
-def install_tool(tool_name, install_command, success_message):
-    user_input = input(Fore.YELLOW + f"Install {tool_name}? (y/n): ")
-    if user_input.lower() == 'y':
-        execute_command(install_command, success_message)
-
-def option_vulnerability_scanners():
-    options = '''    [1] Nmap
-    [2] Sqlmap
-    [3] TMScanner
-    [4] Kali Linux 
-    [5] BACK
-    [6] EXIT'''
-    lol_py(options)
-
-    user_input = input(Fore.YELLOW + "Choose Option: ")
-
-    try:
-        user = int(user_input)
-
-        if user == 1:
-            install_tool("Nmap", "pkg install nmap", "Installation Successful. Type nmap --help for help.")
-        elif user == 2:
-            install_tool("Sqlmap", "cd $HOME && git clone --depth 1 https://github.com/sqlmapproject/sqlmap", "Installation Successful. Type cd sqlmap && python sqlmap.py -h to get Help.")
-        elif user == 3:
-            install_tool("TMScanner", "cd $HOME && apt update && apt upgrade && apt install git python2 && git clone https://github.com/TechnicalMujeeb/TM-scanner.git && cd TM-scanner && chmod +x * && sh install.sh", "Installation Successful. Type cd TM-scanner && python2 tmscanner.py to Start.")
-        elif user == 4:
-            install_tool("Kali Linux", "cd $HOME && apt update && apt upgrade && pkg install wget fish && wget https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-project/raw/master/nethunter-rootless/install-nethunter-termux && chmod +x install-nethunter-termux && ./install-nethunter-termux", '''Installation Successful. Type "nethunter" to start Kali Machine.''')
-        elif user == 5:
-            home()
-        elif user == 6:
-            option_exit()
-        else:
-            display_header()
-            option_vulnerability_scanners()
-    except ValueError:
-        error()
-        option_vulnerability_scanners()
-        
-def option_update_tool():
-    run = '''apt update && apt upgrade
-    wget -O $PREFIX/bin/hacktools https://raw.githubusercontent.com/codepreceptor/HackTools/main/hacktools
-    chmod +x $PREFIX/bin/hacktools
-    clear '''
-    os.system(run)
-    print("\n\n\t[✔️]", update_message, "[✔️]")
-    time.sleep(3)
-    os.system("python3 $PREFIX/opt/HackTools/hacktools.py")
-
-def option_exit():
-    clear_screen()
-    display_header()
-    wait()
-    display_about_creator()
-    wait()
-    thanks = "           THANKS FOR USING MY TOOL.\n"
-    please = "   PLEASE🙏 DON'T FORGET TO JOIN MY YOUTUBE AND TELEGRAM CHANNELS.\n"
-    havean = "              HAVE A NICE DAY😊.\n\n"
-
-    lol_py(thanks)
-    wait()
-    lol_py(please)
-    wait()
-    lol_py(havean)
-    exit()
-
-def wait():
-    time.sleep(1.2)
-
-def error():
-    print(Fore.RED + '''Please Choose Correct Option
-    Please Wait for 3 Seconds''')
-    time.sleep(3)
-    clear_screen()
-    display_header()
-    display_about_creator()
-
-def home():
-    clear_screen()
-    display_header()
-    display_about_creator()
-    option()
-    choose()
+    print(Fore.GREEN+"======================================================\n\n")
 
 def option():
     options = '''    [1] VULNERABILITY SCANNERS (nmap, etc..) 
@@ -126,42 +39,280 @@ def option():
     [6] Instagram Brutal Force 
     [7] UPDATE TOOL
     [0] EXIT'''
+    
+    
     lol_py(options)
 
-def choose():
-    print("\n")
-    user_input = input(Fore.YELLOW + "Choose Option: ")
-
+def option_1():
+    options_1_1 = '''    [1] Nmap
+    [2] Sqlmap
+    [3] TMScanner
+    [4] Kali Linux 
+    [5] BACK
+    [6] EXIT'''
+    lol_py(options_1_1)
+    
+    user_input =  input(Fore.YELLOW+"Choose Option: ")
+    
     try:
         user = int(user_input)
-
+        
         if user == 1:
-            option_vulnerability_scanners()
+            nmap = "pkg install nmap"
+            os.system(nmap)        
+            print("Installation Successful. Type nmap --help for getting help.")
         elif user == 2:
-            option_exploitation_tools()
+            sqlmap = "cd $HOME && git clone --depth 1 https://github.com/sqlmapproject/sqlmap"
+            os.system(sqlmap)
+            print("Installation Successful. Type cd sqlmap && python sqlmap.py -h to get Help.")
         elif user == 3:
-            option_bombing_tools()
+            tmscanner = " cd $HOME && apt update && apt upgrade && apt install git python2 && git clone https://github.com/TechnicalMujeeb/TM-scanner.git && cd TM-scanner && chmod +x * && sh install.sh"
+            os.system(tmscanner)
+            print("Installation Successful. Type cd TM-scanner && python2 tmscanner.py to Start.")
+        
         elif user == 4:
-            option_phishing_tools()
+            kali = "cd $HOME && apt update && apt upgrade && pkg install wget fish && wget https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-project/raw/master/nethunter-rootless/install-nethunter-termux && chmod +x install-nethunter-termux && ./install-nethunter-termux"
+            os.system(kali)
+            print('''Installation Successful. Type "nethunter" to start Kali Machine.''')
         elif user == 5:
-            option_networking_tools()
-        elif user == 6:
-            option_instagram_brutal_force()
-        elif user == 7:
-            option_update_tool()
-        elif user == 0:
-            option_exit()
-        else:
-            print(Fore.RED + '''Please Choose Correct Option
-        Please Wait for 3 Seconds''')
-            time.sleep(3)
-            clear_screen()
             home()
+        elif user == 6:
+            option_0()
+        else:
+            logo()
+            option_1()
+            
     except ValueError:
         error()
-        choose()
-
-if __name__ == "__main__":
-    home()
+        option_1()
+    
+def option_2():
+    options_2_1= '''    [1] Metasploit Framework
+    [2] BACK
+    [0] EXIT'''
+    lol_py(options_2_1)
+    user_input =  input(Fore.YELLOW+"Choose Option: ")
+    try:
+        user = int(user_input)
+        if user == 1:
+            metasploit = ''' cd $HOME && pkg update
+            pkg install wget -y
+            wget https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
+            bash metasploit.sh
+            rm metasploit.sh
+            cd metasploit-framework
+            bundle install '''
+            os.system(metasploit )
             
- 
+        elif user == 0:
+            option_0()
+        
+        elif user == 2:
+            home()
+            
+        else:
+            logo()
+            about_creator()
+            option_2()
+            
+    except ValueError:
+            error()
+            option_2()
+    
+def option_3():
+    options_3_1 = '''    [1] XBomb
+    [2] BACK
+    [3] EXIT
+        '''
+    lol_py(options_3_1)
+    user_input =  input(Fore.YELLOW+"Choose Option: ")
+    try:
+        user = int(user_input)
+        if user == 1:
+            xbomb = "cd $HOME && git clone https://github.com/mr-exo/XBomb.git"
+            os.system(xbomb)
+            print("Installation Successful. Type cd XBomb && pip install -r requirements.txt && python XBomb.py to Start")
+            
+        elif user == 2:
+            home()
+         
+        elif user == 0:
+            option_0()
+        
+        else:
+            logo()
+            about_creator()  
+            option_3()
+            
+    except ValueError:
+            error()
+            option_3()                
+    
+          
+def option_4():
+    options_4_1= '''    [1] Zphisher
+    [2] BACK
+    [0] EXIT        '''
+    lol_py(options_4_1)
+    user_input = input(Fore.YELLOW+"Choose Option: ")
+    try :
+          user = int(user_input)
+          if user == 1:
+              zphisher = '''cd $HOME && pkg install tur-repo
+ pkg install zphisher
+ zphisher''' 
+              os.system(zphisher)
+              print("Installation Successful. Type zphisher to Start")
+          
+          elif user == 2:
+              home()
+           
+          elif user == 0:
+              option_0()
+           
+          else:
+              logo()
+              about_creator()
+              option_4()
+              
+    except ValueError:
+            error()
+            option_4()                               
+          
+def option_5():
+    options_5_1 = '''    [1] IP-Tracer
+    [2] BACK
+    [0] EXIT        '''
+    lol_py(options_5_1)
+    
+    user_input = input(Fore.YELLOW+"Choose Option: ")
+    try :
+          user = int(user_input)
+          if user == 1:
+              ip = '''cd $HOME && git clone https://github.com/rajkumardusad/IP-Tracer.git
+cd IP-Tracer
+chmod +x install
+sh install '''
+
+
+                                          
+              os.system(ip)
+              print("Installation Successful. Type trace -t target-ip to Trace.")
+          elif user == 2:
+             home()
+             
+          elif user == 0:
+             option_0()
+             
+          else:
+             logo()
+             about_creator()
+             option_5()
+
+    except ValueError:
+            error()
+            option_5()   
+
+def option_6():
+    run = f'''cd $HOME
+    apt update && apt upgrade
+    apt install git python python3
+    git clone https://github.com/Bitwise-01/Instagram.git
+    pip install pyproject.toml numpy 
+    pip3 install pyproject.toml numpy
+    pip install pipenv asdf
+    pip3 install pipenv asdf
+    cd Instagram
+    wget https://raw.githubusercontent.com/codepreceptor/Instagram/main/pass_list.txt
+    wget https://raw.githubusercontent.com/codepreceptor/Instagram/main/proxy.txt '''
+    os.system(run)
+    print('''\n\n\t [✔️]Installation Successful, Type cd Instagram and then pipenv shell and then python instagram.py -u {username} -p pass_list.txt -px proxy.txt''')
+            
+
+
+
+def option_7():
+    run = f'''apt update && apt upgrade
+    wget -O $PREFIX/bin/hacktools https://raw.githubusercontent.com/codepreceptor/HackTools/main/hacktools
+    chmod +x $PREFIX/bin/hacktools
+clear '''
+    
+    os.system(run)
+    print("\n\n\t[✔️]",update,"[✔️]")
+    time.sleep(3)
+    os.system("python3 $PREFIX/opt/HackTools/hacktools.py")
+def option_0():
+    os.system("clear")
+    print ("\n\n")
+    logo()
+    wait()
+    about_creator()
+    wait()
+    thanks = (f"                      THANKS FOR USING MY TOOL.\n")
+    please = (f"      PLEASE🙏 DON'T FORGOT TO JOIN MY YOUTUBE AND TELEGRAM CHANNELS.\n")
+    havean = (f"                      HAVE A NICE DAY😊.\n\n") 
+    
+    lol_py(thanks)
+    wait()
+    lol_py(please)
+    wait ()
+    lol_py(havean)
+    
+    exit()
+    
+    
+def choose():
+    print("\n")
+    user_input = input(Fore.YELLOW+"Choose Option: ")
+    
+    try:
+        user = int(user_input)
+        
+        if user == 1:
+            option_1()
+        elif user == 2:
+            option_2()
+        elif user == 3:
+            option_3()
+        elif user == 4:
+            option_4()
+        elif user == 5:
+            option_5()
+        elif user == 6:
+            option_6()
+        elif user == 7:
+            option_7()
+        elif user == 0:
+            option_0()
+        else:
+            print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+            time.sleep(3)
+            os.system("clear")
+            home() 
+    except ValueError:
+        print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+        time.sleep(3)
+        os.system("clear")
+        home()
+    
+        
+def home():
+    clear()
+    logo()
+    about_creator()
+    option()
+    choose()                          
+
+def error():
+        print(Fore.RED+'''Please Choose Correct Option
+        Please Wait for 3 Seconds''')
+        time.sleep(3)
+        os.system("clear")
+        logo()
+        about_creator()    
+    
+
+home() 
+    
